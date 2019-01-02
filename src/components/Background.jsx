ec57113791aa7fc15204f99a9eb9313e92750121
s0njablade <sonjita.chacon@gmail.com>
2019-01-02 11:27:01 -0700

    first commit, create React app, added a background image and linked to bootstrap

---
+++ src/components/Background.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Background extends React.Component {
+
+    render() {
+        return (
+            <div className="Background">
+
+            </div>
+        )
+    }
+}
+export default Background;
